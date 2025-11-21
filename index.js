874528cfe1545cb0e169ab1558da303c124a1c09
LauraT1989 <142106565+LauraT1989@users.noreply.github.com>
2025-11-21 16:51:03 +0000

    updated js message

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 var myname="Laura";
 var yourname= prompt("What is your name?");
-alert("Hi "+ yourname +"! My name is " + myname + ". Welcome to my website! ");
+var first_letter=yourname_char.slice(0,1);
+var next_letters=yourname_char.slice(1,yourname_char.length);
+var full_name=first_letter.toUpperCase()+next_letters.toLowerCase;
+alert("Hi "+ yourname +"! My name is " + myname + ". Welcome to my website!");
