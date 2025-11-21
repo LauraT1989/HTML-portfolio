08df8aa9b96255eb745d7556284cf48c12ce846f
LauraT1989 <142106565+LauraT1989@users.noreply.github.com>
2025-11-21 16:53:53 +0000

    updated error in js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var myname="Laura";
 var yourname= prompt("What is your name?");
-var first_letter=yourname_char.slice(0,1);
-var next_letters=yourname_char.slice(1,yourname_char.length);
+var first_letter=yourname.slice(0,1);
+var next_letters=yourname.slice(1,yourname.length);
 var full_name=first_letter.toUpperCase()+next_letters.toLowerCase;
 alert("Hi "+ yourname +"! My name is " + myname + ". Welcome to my website!");
